63781916ba8fe932d64c9e477a0ec18f602ea1ca
sachidanandaDhal <dhal.sachidananda1@gmail.com>
2024-04-21 20:05:14 +0530

    bubu

--- src/components/home/user-details.js
+++ src/components/home/user-details.js
@@ -1,130 +1,110 @@
-import { OmniStyleElement, OmniTableElement, html, css } from 'omni-ui';
+
+import { OmniStyleElement, html, css } from 'omni-ui';
 import './user-data.js';
 import './edit-user.js'; 
 import '../form/user-form.js';
+
 export default class UserDetails extends OmniStyleElement {
   static get styles() {
     return [
       super.styles,
       css`
         omni-table::part(table-body-cell) {
-          transition: color 0.25s ease-in-out,
-            background-color 0.25s ease-in-out;
+          transition: color 0.25s ease-in-out, background-color 0.25s ease-in-out;
         }
         omni-table::part(table-header-actions) {
           padding-left: 85px !important;
         }
-      
       `,
     ];
   }
 
   static get properties() {
     return {
-    selectedUser: { type: Object },
-    editdUser: { type: Object },
+      selectedUser: { type: Object },
+      editdUser: { type: Object },
+      users: { type: Array },
+      columns: { type: Array },
+      data: { type: Array }
     };
   }
+
   constructor() {
     super();
-    const userData = JSON.parse(localStorage.getItem("userData"));
-    this.users = userData || [];
+    this.users = JSON.parse(localStorage.getItem("userData")) || [];
     this.columns = [
-      // { label: "ID", key: "id", isSortable: true },
       { label: "Name", key: "fullName", isSortable: true },
       { label: "Contact Number", key: "phoneNumber", isSortable: true },
       { label: "Gender", key: "gender", isSortable: true },
-      {
-        label: "Office Email",
-        key: "officeEmail",
-        isSortable: false,
-      },
-      {
-        label: "Role",
-        key: "role",
-        isSortable: false,
-      },
+      { label: "Office Email", key: "officeEmail", isSortable: false },
+      { label: "Role", key: "role", isSortable: false },
       { label: "Actions", key: "actions" },
     ];
+    this.refreshData();
+  }
+
+  refreshData() {
     this.data = this.users.map(user => ({
       id: user.id,
       fullName: `${user.personal_details.first_name} ${user.personal_details.last_name}`,
       phoneNumber: `${user.contact_details.phoneNumber}`,
-      gender:`${user.personal_details.gender}`,
+      gender: `${user.personal_details.gender}`,
       officeEmail: `${user.user_login_details.officeEmail}`,
       role: `${user.user_login_details.role}`,
-
       actions: html`
-      <omni-style>
-      <td part="table-body-cell ">
+        <omni-style>
+          <td part="table-body-cell">
             <omni-tooltip>
               <button slot="invoker" @click="${() => this.deleteUser(user)}">
-                <omni-icon
-                  class="is-size-2"
-                  icon-id="omni:interactive:delete"
-                ></omni-icon>
+                <omni-icon class="is-size-2" icon-id="omni:interactive:delete"></omni-icon>
               </button>
               <div slot="content">Delete</div>
             </omni-tooltip>
             <omni-tooltip>
               <button slot="invoker" @click="${() => this.viewUser(user)}">
-                <omni-icon
-                  class="is-size-2"
-                  icon-id="omni:interactive:launch"
-                ></omni-icon>
+                <omni-icon class="is-size-2" icon-id="omni:interactive:launch"></omni-icon>
               </button>
               <div slot="content">View Data</div>
             </omni-tooltip>
             <omni-tooltip>
-              <button slot="invoker"  @click="${() => this.editUser(user)}">
-                <omni-icon
-                  class="is-size-2"
-                  icon-id="omni:interactive:edit"
-                ></omni-icon>
+              <button slot="invoker" @click="${() => this.editUser(user)}">
+                <omni-icon class="is-size-2" icon-id="omni:interactive:edit"></omni-icon>
               </button>
               <div slot="content">Edit Data</div>
-              </omni-tooltip>
+            </omni-tooltip>
           </td>
-          </omni-style>
-        
+        </omni-style>
       `
     }));
-
-    this.selectedUser = null;
-    this.editdUser = null;
   }
 
-
   deleteUser(user) {
-    console.log("Deleting user:", user);
     const index = this.users.findIndex(u => u.id === user.id);
     if (index !== -1) {
       this.users.splice(index, 1);
       localStorage.setItem("userData", JSON.stringify(this.users));
-      this.data = this.data.filter(u => u.id !== user.id);
+      this.refreshData();
       this.requestUpdate();
     }
   }
   
   viewUser(user) {
-    console.log("Viewing user:", user);
     this.selectedUser = user;
-    console.log("Viewing user:", this.selectedUser);
   }
+
   closeUserData() {
     this.selectedUser = null; 
   }
 
   editUser(user) {
     this.editdUser = user;
-    console.log(this.editdUser);
-  }
-  closeEditUser() {
-    this.editdUser = null; 
-    this.data = this.loadUserData();
-   
   }
 
+  closeEditUser() {
+    this.editdUser = null; 
+    this.refreshData();
+    this.requestUpdate();
+  }
 
   render() {
     return html`
@@ -140,15 +120,14 @@ export default class UserDetails extends OmniStyleElement {
               @close-user-data="${this.closeUserData}"
             ></user-data>`
           : ""}
-          ${this.editdUser
-        ? html`
-            <edit-user
-              .userData="${ this.editdUser }"
-              @close-edit-user="${this.closeEditUser}"
-              @save-edited-data="${this.handleUpdate}"
-            ></edit-user>
-          `
-        : ""}
+        ${this.editdUser
+          ? html`
+              <edit-user
+                .userData="${ this.editdUser }"
+                @close-edit-user="${this.closeEditUser}"
+              ></edit-user>
+            `
+          : ""}
       </omni-style>
     `;
   }
@@ -174,11 +153,6 @@ export default class UserDetails extends OmniStyleElement {
       return 0;
     });
   }
-
-  
-  
-
-  
 }
 
 customElements.define("user-details", UserDetails);
